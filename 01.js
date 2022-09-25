9443c23b800250fbef1036a6fc60baef7a3708ab
Vinay <vinaytiwari1501@gmail.com>
2022-09-25 21:03:28 +0530

    Synchronous & asyncnous programming

---
+++ 01.js
@@ -0,0 +1,13 @@
+//synchronous programming vs asynchronous programming
+//synchronous programming
+
+// console.log("script start");
+
+// for(let i = 1; i< 10000; i++){
+//     console.log("inside for loop");
+// }
+
+// console.log("script end");
+
+//javascript is a synchronous programming & single-threaded
+
